027ba52af44c1e1bedf07eb1d99e29ceb233e81c
tomas-gonzalez <tomas-gq@hotmail.com>
2020-11-05 17:41:47 -0300

    Continuación ahorcado
    Seguimos haciendo el esqueleto principal del programa

--- ahorcado.py
+++ ahorcado.py
@@ -68,10 +68,16 @@ def random_word():
     index = random.randint(0, len(PALABRAS)-1)
     return PALABRAS[index]
 
+def display(Hword, attempts):
+    print(IMAGES[attempts] +"\n")
+    pri
 def run():
     word = random_word()
-    hidden_word= ['-'] * len(word)
-    pass
+    Hword= ['-'] * len(word)
+    attempts= 0
+
+    while True:
+        display(hword, attempts)
 
 if __name__ == '__main__':
     print('¡ B I E N V E N I D O !')
