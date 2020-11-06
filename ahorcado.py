a968b9ef1bbf236af78fa830793f72f735998c7e
tomas-gonzalez <tomas-gq@hotmail.com>
2020-11-05 21:24:46 -0300

    Ahorcado terminado

--- ahorcado.py
+++ ahorcado.py
@@ -52,8 +52,6 @@ IMAGES = ['''
     =========''']
 
 PALABRAS = [
-    'esternocleidomaistoideo',
-    'electroencefalograma',
     'gato',
     'rayo',
     'cuchillo',
@@ -74,17 +72,38 @@ def display(Hword, attempts):
     print(Hword)
     print('.-.-.-.-.-.-.-.-.-.')
 
-
-
-
 def run():
     word = random_word()
     Hword= ['-'] * len(word)
     attempts= 0
-
     while True:
         display(Hword, attempts)
+        index_letras=[]
         letra= str(input('Escoge una letra:'))
+        for i in range (len(word)):
+            if word[i] == letra:
+                index_letras.append(i)
+        if len(index_letras) == 0:
+            attempts += 1
+
+            if attempts == 7:
+                display(Hword, attempts)
+                print('')
+                print('¡Perdiste! La palabra correcta era {}'.format(word))
+                break
+        else:
+            for i in index_letras:
+                Hword[i] = letra
+
+            index_letras = []
+
+        try:
+            Hword.index('-')
+        except ValueError:
+            print('')
+            print('¡Felicidades! Ganaste. La palabra es: {}'.format(word))
+            break
+
 
 if __name__ == '__main__':
     print('¡ B I E N V E N I D O !')
